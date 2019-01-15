a740f5cd5395118be1bf3d342af0fb4ca287400c
pittpir <pittpir@gmail.com>
2019-01-15 15:04:46 -0600

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ let express = require('express');
 
 let app = express();
 
+app.get("/tip/:total/:tipPercentage", (request,response) => {
+	let moo = (request.params.total * ((request.params.tipPercentage/100)+1));
+	response.send(`Bill total = ${moo}`);	
+});
 
 
 app.get("/greetings/", (request,response) => {
@@ -13,5 +17,6 @@ app.get("/greetings/", (request,response) => {
 app.get("/greetings/:name", (request,response) => {
 	response.send(`Hello ${request.params.name}.  Nice to see you\n`);
 });
+
 
 app.listen(9000);
