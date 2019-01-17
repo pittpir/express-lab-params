349e6d43c7ce32718a313b900f2ce94fdfe63a62
pittpir <pittpir@gmail.com>
2019-01-17 09:01:06 -0600

    Commit 4: Fibonacci application created.

--- server.js
+++ server.js
@@ -29,6 +29,24 @@ app.get("/magic/:question", (request,response) => {
 
 });
 
+//is this a Fibonacci number
+//A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square
+app.get("/fib/isfib/:num", (request,response) => {
+
+	let n = request.params.num;
+
+	let x = (5*n*n + 4); 
+    let y = (5*n*n - 4);
+
+    if ( ((Math.sqrt(x) % 1) === 0) || ((Math.sqrt(y) % 1) === 0) ) {
+    	response.send(`${n} is a Fibonacci number\n`);
+    } else {
+    	response.send(`${n} is NOT a Fibonacci number\n`);
+    }
+});
+
+
+//calculate a Fibonacci number to a given index where n of 4 equals 5
 app.get("/fib/:index", (request,response) => {
 
 	let index = request.params.index;
@@ -37,7 +55,7 @@ app.get("/fib/:index", (request,response) => {
 	let t2 = 1;
 	let next = 0;
 	
-	for (let i=2; i<=index; i++) {
+	for (let i=1; i<=index; i++) {
 		next = t1 + t2;
 		t1 = t2;
 		t2 = next;
@@ -45,5 +63,8 @@ app.get("/fib/:index", (request,response) => {
 	response.send(`Fib number at index(${index}) is:  ${next} \n`);
 });
 
-
-app.listen(9000);
+// LISTENER
+let port = 9000;
+app.listen(port, function() {
+  console.log('NodeJS running on port: ', port);
+})
